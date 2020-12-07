c9d609f1f818980a2b81e813b4fe88ab55e48517
kolron <74140353+kolron@users.noreply.github.com>
2020-12-07 09:14:58 +0200

    Add files via upload

---
+++ Ex2/myBank.h
@@ -0,0 +1,22 @@
+#ifndefine MYBANK_H
+#define MYBANK_H
+
+void openAccount(double n);
+
+double checkBalance(int n);
+
+void deposit(int n, double a);
+
+void withdraw(int n, double w);
+
+void close(int n);
+
+void interest(double i);
+
+void print();
+
+void exit();
+
+void menu();
+
+#endif
